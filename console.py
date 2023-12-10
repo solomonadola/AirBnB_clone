ec3432e5d516a65b2c716eeb1dc5003dc5368b9e
solomonadola <solomonadola84@gmail.com>
2023-12-10 18:54:22 +0300

    simple code that passes pycodestyle

---
+++ console.py
@@ -0,0 +1,10 @@
+""" this module is just to test pycodestyle
+the rest will follow after this one"""
+
+
+def pycodestyle():
+    print("this is a code that passes pycodestyle")
+
+
+if __name__ == "__main__":
+    pycodestyle()
