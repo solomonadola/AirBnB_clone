f5c5c96aa6724292c10cca49d68692b0f3cd8fe1
solomonadola <solomonadola84@gmail.com>
2023-12-12 14:02:43 +0300

    File storage added

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,7 @@ implements the serialization and deserialization of instances to a JSON file.
 
 from datetime import datetime
 import uuid
+import models
 
 
 class BaseModel:
@@ -37,12 +38,13 @@ class BaseModel:
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
                         value = datetime.\
-                            strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                            strptime(value, date_f)
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -58,6 +60,7 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
---
+++ models/engine/file_storage.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Module for FileStorage class.
+
+This module defines the FileStorage class, responsible for serializing instances to a JSON file
+and deserializing a JSON file to instances. It provides methods to manage a dictionary (__objects)
+that stores instances with keys formatted as '<class name>.<id>'.
+
+Attributes:
+    __file_path (str): The path to the JSON file.
+    __objects (dict): A dictionary to store instances by keys.
+
+Methods:
+    all(self): Returns the dictionary __objects.
+    new(self, obj): Sets in __objects the obj with key <obj class name>.id.
+    save(self): Serializes __objects to the JSON file (path: __file_path).
+    reload(self): Deserializes the JSON file to __objects (only if the JSON file (__file_path) exists).
+"""
+
+import json
+from models.base_model import BaseModel
+
+class FileStorage:
+    """
+    This class serializes instances to a
+    JSON file and deserializes JSON file to instances.
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects."""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id."""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)."""
+        obj_dict = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
+        for key, value in FileStorage.__objects.items():
+            obj_dict[key] = value.to_dict()
+        with open(FileStorage.__file_path, 'w') as file:
+            json.dump(obj_dict, file)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists)."""
+        try:
+            with open(FileStorage.__file_path, 'r') as file:
+                obj_dict = json.load(file)
+                for key, value in obj_dict.items():
+                    class_name, obj_id = key.split('.')
+                    obj = eval(class_name)(**value)
+                    FileStorage.__objects[key] = obj
+        except FileNotFoundError:
+            pass
