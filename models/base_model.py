2e4a8efba862e277be54670df3b1107113db229d
solomonadola <solomonadola84@gmail.com>
2023-12-12 11:02:02 +0300

    Task 3 base model setup

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-A module that defines a base class for other classes.
+Thsi is a module that defines a base class for other classes.
 
 This module contains the BaseModel class, which provides common attributes
 and methods for other classes in the models package. The BaseModel class
 implements the serialization and deserialization of instances to a JSON file.
 """
 
-import uuid
 from datetime import datetime
+import uuid
 
 
 class BaseModel:
@@ -29,22 +29,20 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """
-        initializes object using dictionary if given otherwise
-        it gives default value
+        initializes a new instance of the BaseModel class.
         """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    setattr(self, key, value)
-            self.created_at = datetime.fromisoformat(kwargs['created_at'])
-            self.updated_at = datetime.fromisoformat(kwargs['updated_at'])
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
+        """
+        Return a string representation of the BaseModel instance.
+
+        Returns:
+                str: A string containing the class name, id,
+                and object's dictionary
+        """
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
 
@@ -53,9 +51,15 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """returns a dictionary containing all key/value of __dict__
-        of the instance"""
-        dic = vars(self).copy()
+        """
+        Return a dictionary representation of the object for serialization.
+
+        Returns:
+            dict: A dictionary containing all keys/values of the instance.
+                  Includes '__class__', 'created_at', and 'updated_at'.
+
+        """
+        dic = self.__dict__.copy()
         dic['__class__'] = self.__class__.__name__
         dic['updated_at'] = self.updated_at.isoformat()
         dic['created_at'] = self.created_at.isoformat()
