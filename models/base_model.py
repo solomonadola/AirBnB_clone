b1f51b32ec7a965585f939c5e88154bed9c9282c
solomonadola <solomonadola84@gmail.com>
2023-12-12 19:59:34 +0300

    File storage test added

--- models/base_model.py
+++ models/base_model.py
@@ -37,9 +37,13 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
-                        value = datetime.\
-                            strptime(value, date_f)
-                    setattr(self, key, value)
+                        if isinstance(value, datetime):
+                            setattr(self, key, value)
+                        else:
+                            setattr(self, key,
+                                    datetime.strptime(value, date_f))
+            if 'id' in kwargs:
+                self.id = kwargs['id']
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,80 @@
+import unittest
+import os
+import json
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print('Setting up TestFileStorage class...')
+
+    @classmethod
+    def tearDownClass(cls):
+        print('Tearing down TestFileStorage class...')
+        # Clean up by removing the test JSON file if it exists
+        test_file_path = "test_file.json"
+        if os.path.exists(test_file_path):
+            os.remove(test_file_path)
+
+    def setUp(self):
+        print('Setting up a test...')
+        self.file_storage = FileStorage()
+        self.base_model = BaseModel()
+        self.file_path = "test_file.json"
+        self.file_storage._FileStorage__file_path = self.file_path
+        self.file_storage._FileStorage__objects = {}
+
+    def tearDown(self):
+        print('Tearing down a test...')
+
+    def test_all_method(self):
+        # Add an object to __objects before calling all()
+        key = "{}.{}".format(self.base_model.__class__.__name__,
+                             self.base_model.id)
+        self.file_storage._FileStorage__objects[key] = self.base_model
+
+        all_objects = self.file_storage.all()
+        self.assertIsInstance(all_objects, dict)
+        self.assertEqual(all_objects, self.file_storage._FileStorage__objects)
+
+    def test_new_method(self):
+        key = "{}.{}".format(self.base_model.__class__.__name__,
+                             self.base_model.id)
+        self.file_storage.new(self.base_model)
+        self.assertIn(key, self.file_storage._FileStorage__objects)
+
+    def test_save_method(self):
+        self.file_storage.new(self.base_model)
+        self.file_storage.save()
+
+        # Check if the file has been created
+        self.assertTrue(os.path.exists(self.file_path))
+
+        # Check if the content of the file is correct
+        with open(self.file_path, 'r') as file:
+            saved_data = json.load(file)
+            expected_key = "{}.{}".format(self.base_model.__class__.__name__,
+                                          self.base_model.id)
+            self.assertIn(expected_key, saved_data)
+
+    def test_reload_method(self):
+        # Save an object to a test file
+        key = "{}.{}".format(self.base_model.__class__.__name__,
+                             self.base_model.id)
+        self.file_storage._FileStorage__objects[key] = self.base_model
+        with open(self.file_path, 'w') as file:
+            json.dump({key: self.base_model.to_dict()}, file)
+
+        # Reload the data and check if it matches the saved object
+        self.file_storage.reload()
+        reloaded_objects = self.file_storage.all()
+        self.assertIn(key, reloaded_objects)
+        reloaded_model = reloaded_objects[key]
+        self.assertEqual(reloaded_model.id, self.base_model.id)
+
+
+if __name__ == '__main__':
+    unittest.main()
