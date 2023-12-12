9f44405920d5b1510b07b7730064ee5e41072614
solomonadola <solomonadola84@gmail.com>
2023-12-12 19:53:44 +0300

    File storage test added

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -28,36 +28,31 @@ class FileStorage:
     This class serializes instances to a
     JSON file and deserializes JSON file to instances.
     """
-    __file_path = "file.json"
-    __objects = {}
+    def __init__(self):
+        self.__file_path = "file.json"
+        self.__objects = {}
 
     def all(self):
-        """Returns the dictionary __objects."""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id."""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)."""
-        fsto = FileStorage.__objects.items()
-        obj_dict = {key: obj.to_dict() for key, obj in fsto}
-        for key, value in FileStorage.__objects.items():
-            obj_dict[key] = value.to_dict()
-        with open(FileStorage.__file_path, 'w') as file:
+        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, 'w') as file:
             json.dump(obj_dict, file)
 
     def reload(self):
-        """Deserializes the JSON file to __objects
-        (only if the JSON file (__file_path) exists)."""
         try:
-            with open(FileStorage.__file_path, 'r') as file:
+            with open(self.__file_path, 'r') as file:
                 obj_dict = json.load(file)
                 for key, value in obj_dict.items():
                     class_name, obj_id = key.split('.')
-                    obj = eval(class_name)(**value)
-                    FileStorage.__objects[key] = obj
+                    obj_class = globals().get(class_name)
+                    if obj_class and issubclass(obj_class, BaseModel):
+                        obj = obj_class(**value)
+                        self.__objects[key] = obj
         except FileNotFoundError:
             pass
