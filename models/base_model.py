bb22e74a02d59460839dccbb24f241c1f8bd90a1
solomonadola <solomonadola84@gmail.com>
2023-12-12 11:59:55 +0300

    Base model args and kwargs added

--- models/base_model.py
+++ models/base_model.py
@@ -27,13 +27,21 @@ class BaseModel:
         a __class__ key with the class name.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         initializes a new instance of the BaseModel class.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        setattr(self, key, datetime.datetime.fromtimestamp(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
